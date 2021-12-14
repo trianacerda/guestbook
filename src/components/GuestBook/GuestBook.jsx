02444e6f60d2e04656575428a2b94b18ab47fd3e
Triana Cerda <trianaRcerda@gmail.com>
2021-12-14 14:57:26 -0800

    added textarea and btn text + rendering to page

--- src/components/GuestBook/GuestBook.jsx
+++ src/components/GuestBook/GuestBook.jsx
@@ -20,24 +20,42 @@ function GuestBook() {
     <>
       <h1>Display Message to Guest</h1>
       <form>
-        <label className="name-label" htmlFor="name-entry">
-          Name:
-          <input
-            type="text"
-            id="guest-name"
-            value={name}
-            className="name-input"
-            placeholder="Enter Name"
-            onChange={(e) => setName(e.target.event)}
-          />
-        </label>
+        <div className="name-field">
+          <label className="name-label" htmlFor="name-entry">
+            Name:
+            <input
+              type="text"
+              id="guest-name"
+              value={name}
+              className="name-input"
+              placeholder="Enter Name"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+        </div>
+        <div className="message-field">
+          <label>
+            Message:
+            <textarea
+              className="message-text-area"
+              value={guestMessage}
+              id="guest-message"
+              placeholder="Please leave a message..."
+            >
+              onChange={(e) => setGuestMessage(e.target.value)}
+            </textarea>
+          </label>
+        </div>
+
         <button
           type="submit"
           className="submit-btn"
           onClick={() => {
             setName('');
           }}
-        ></button>
+        >
+          Sign Your Message
+        </button>
       </form>
     </>
   );
