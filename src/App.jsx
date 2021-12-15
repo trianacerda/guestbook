7c469da01140374fdcb3093a63050c78678e679d
Triana Cerda <trianaRcerda@gmail.com>
2021-12-14 18:05:16 -0800

    added MessageList component

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import Home from './views/Home/Home';
 import LayoutViews from './views/LayoutViews/LayoutViews';
 
 export default function App() {
-  return;
-  <LayoutViews>
-    <Home />;
-  </LayoutViews>;
+  return (
+    <LayoutViews>
+      <Home />
+    </LayoutViews>
+  );
 }
---
+++ src/components/MessageList/MessageList.jsx
@@ -0,0 +1,22 @@
+import { useMessages } from '../../context/MessageContext';
+import Message from '../Message/Message';
+
+function MessageList() {
+  const { messages } = useMessages();
+
+  return (
+    <>
+      <ul>
+        {messages.map((message) => {
+          return (
+            <li key={`${message.name}-${message.message}`}>
+              <Message message={message} />
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
+
+export default MessageList;
