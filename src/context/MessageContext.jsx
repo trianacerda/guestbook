3d8e9c55224162af9576823bb102c7b038dd6157
Triana Cerda <trianaRcerda@gmail.com>
2021-12-14 16:38:44 -0800

    added MessageProvider to context

---
+++ src/context/MessageContext.jsx
@@ -0,0 +1,27 @@
+import { useContext, createContext, useState } from 'react';
+
+const MessageContext = createContext();
+
+const MessageProvider = ({ children }) => {
+  const [messages, setMessages] = useState('');
+
+  return (
+    <MessageContext.Provider value={{ messages, setMessages }}>
+      {children}
+    </MessageContext.Provider>
+  );
+};
+
+const useMessages = () => {
+  const context = useContext(MessageContext);
+
+  if (context === undefined) {
+    throw new Error(
+      'useMessages hook must be used withing a MessageContext Provider parent tag'
+    );
+  }
+
+  return context;
+};
+
+export { MessageProvider, useMessages };
