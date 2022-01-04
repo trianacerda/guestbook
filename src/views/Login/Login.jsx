2bc97b439b97639ae827b97717ccb273b1e0666c
Triana Cerda <trianaRcerda@gmail.com>
2022-01-04 14:57:45 -0800

    added snapshot test for Login comp

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useState } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 import { useAuth } from '../../hooks/useAuth';
---
+++ src/views/Login/Login.test.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import Login from '../Login/Login';
+import { UserProvider } from '../../context/UserContext';
+import { ProvideAuth } from '../../context/ProvideAuth';
+
+it('should render Login comp + snapshot test', async () => {
+  const { container } = render(
+    <MemoryRouter>
+      <UserProvider>
+        <ProvideAuth>
+          <Login />
+        </ProvideAuth>
+      </UserProvider>
+    </MemoryRouter>
+  );
+
+  expect(container).toMatchSnapshot();
+});
