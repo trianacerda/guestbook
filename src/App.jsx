9a72e39a4e443b6a2f140533421f610dc8fc7af5
Triana Cerda <trianaRcerda@gmail.com>
2022-01-03 16:18:45 -0800

    added PrivateRoute file

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import Home from './views/Home/Home';
 import LayoutViews from './views/LayoutViews/LayoutViews';
 import { PrivateRoute } from '';
+import { ProvideAuth } from './context/ProvideAuth';
 
 export default function App() {
   return (
-    <ProvideAuth>
-      <Router>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <PrivateRoute path="/guestbook">
-          <LayoutViews>
-            <Home />
-          </LayoutViews>
-        </PrivateRoute>
-      </Router>
-    </ProvideAuth>
+    <>
+      <ProvideAuth>
+        <Router>
+          <Switch>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <PrivateRoute path="/guestbook">
+              <LayoutViews>
+                <Home />
+              </LayoutViews>
+            </PrivateRoute>
+          </Switch>
+        </Router>
+      </ProvideAuth>
+    </>
   );
 }
